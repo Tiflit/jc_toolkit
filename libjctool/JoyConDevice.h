33d77694d280da28e39d9d74a527330be653d6ab
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-22 14:24:16 -0400

    Initialize StickCalibration members in JoyConDevice
    Initialize leftCal and rightCal with default values.

--- libjctool/JoyConDevice.h
+++ libjctool/JoyConDevice.h
@@ -1,4 +1,38 @@
+#pragma once
+#include <cstdint>
+#include <string>
+
+struct StickCalibration {
+    int16_t minX, centerX, maxX;
+    int16_t minY, centerY, maxY;
+};
+
+struct StickState {
+    float x; // normalized -1.0 to 1.0
+    float y;
+};
+
+class JoyConDevice {
+public:
+    JoyConDevice();
+    ~JoyConDevice();
+
+    bool connect();
+    void disconnect();
+    bool isConnected() const;
+
+    StickCalibration getLeftCalibration();
+    StickCalibration getRightCalibration();
+
+    StickState getLeftStick();
+    StickState getRightStick();
+
 private:
-    StickCalibration leftCal;
-    StickCalibration rightCal;
+    StickCalibration leftCal{};
+    StickCalibration rightCal{};
     bool calLoaded = false;
+
+    StickState computeStick(const StickCalibration& cal,
+                            uint16_t rawX,
+                            uint16_t rawY) const;
+};
