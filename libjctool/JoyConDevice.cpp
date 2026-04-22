acb9fd5a3c9df5edb2eabf68ba053e1e8a89c069
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-22 14:13:36 -0400

    Add JoyConDevice class with connection methods
    Implement JoyConDevice methods for connection and calibration

--- libjctool/JoyConDevice.cpp
+++ libjctool/JoyConDevice.cpp
@@ -1,1 +1,54 @@
+#include "JoyConDevice.h"
+#include "Spi.h"
+#include "../jctool/jctool.h"   // temporary dependency, will be removed later
 
+JoyConDevice::JoyConDevice() {
+}
+
+JoyConDevice::~JoyConDevice() {
+    disconnect();
+}
+
+bool JoyConDevice::connect() {
+    // Temporary: reuse the old device_connection() function
+    int res = device_connection();
+    return (res == 0);
+}
+
+void JoyConDevice::disconnect() {
+    // TODO: close HID handle properly once extracted
+}
+
+bool JoyConDevice::isConnected() const {
+    return check_connection_ok;
+}
+
+StickCalibration JoyConDevice::getLeftCalibration() {
+    uint8_t buf[22] = {};
+    jc_get_spi_data(0x8010, sizeof(buf), buf);
+
+    StickCalibration cal{};
+    // TODO: decode using jc_decode_stick_params()
+    return cal;
+}
+
+StickCalibration JoyConDevice::getRightCalibration() {
+    uint8_t buf[22] = {};
+    jc_get_spi_data(0x801B, sizeof(buf), buf);
+
+    StickCalibration cal{};
+    // TODO: decode using jc_decode_stick_params()
+    return cal;
+}
+
+StickState JoyConDevice::getLeftStick() {
+    StickState s{};
+    // TODO: implement raw->normalized conversion
+    return s;
+}
+
+StickState JoyConDevice::getRightStick() {
+    StickState s{};
+    // TODO: implement raw->normalized conversion
+    return s;
+}
